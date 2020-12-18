b1207f0532740d852dd5c15a6ec44e38c497d01a
kei-yokoyama <74888936+kei-yokoyama@users.noreply.github.com>
2020-12-18 23:01:00 +0900

    ユーザー単体 テスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :nickname, presence: true
   validates :email, presence: true
-  validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: '英語、数字 それぞれ1文字以上を入力してください' }
+  validates :password, presence: true,
+                       format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: '英語、数字 それぞれ1文字以上を入力してください' }
   validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '漢字、全角ひらがな、全角カタカナ を入力してください' }
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '漢字、全角ひらがな、全角カタカナ を入力してください' }
   validates :family_name_kana, presence: true, format: { with: /\A([ァ-ン]|ー)+\z/, message: '全角カタカナ を入力してください' }
   validates :first_name_kana, presence: true, format: { with: /\A([ァ-ン]|ー)+\z/, message: '全角カタカナ を入力してください' }
+  validates :birthday, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,48 +1,133 @@
 require 'rails_helper'
 RSpec.describe User, type: :model do
-
   describe User do
     before do
       @user = FactoryBot.build(:user)
     end
 
-  describe 'ユーザー新規登録' do
-    context '新規登録がうまくいくとき' do
-      it 'nicknameとemail,password,password確認、名字、名前、名字カナ、名前カナ、生年月日が存在すれば登録' do
-        expect(@user).to be_valid
+    describe 'ユーザー新規登録' do
+      context '新規登録がうまくいくとき' do
+        it 'nicknameとemail,password,password確認、名字、名前、名字カナ、名前カナ、生年月日が存在すれば登録' do
+          expect(@user).to be_valid
+        end
       end
-      
 
-    end
+      context '新規登録がうまくいかないとき' do
+        it 'nicknameが空' do
+          @user.nickname = nil
+          @user.valid?
+          expect(@user.errors.full_messages).to include("Nickname can't be blank")
+        end
 
-    context '新規登録がうまくいかないとき' do
-      it 'nicknameが空' do
-        @user.nickname = nil
-        @user.valid?
-        binding.pry
-        expect(user).to incrude ("Nickname can't be blank")
+        it 'emailが空' do
+          @user.email = nil
+          @user.valid?
+          expect(@user.errors.full_messages).to include("Email can't be blank")
+        end
+
+        it 'emailに@なし' do
+          @user.email = 'abc'
+          @user.valid?
+          expect(@user.errors.full_messages).to include('Email is invalid')
+        end
+
+        it 'emailが別アカウントにすでに存在' do
+          another_user = FactoryBot.create(:user)
+          @user.email = another_user.email
+          @user.valid?
+          expect(@user.errors.full_messages).to include('Email has already been taken')
+        end
+
+        it 'passwordが空' do
+          @user.password = nil
+          @user.valid?
+          expect(@user.errors.full_messages).to include("Password can't be blank")
+        end
+
+        it 'passwordが5文字以下' do
+          @user.password = 'a1234'
+          @user.valid?
+          expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
+        end
+
+        it 'passwordが英数字混合でない' do
+          @user.password = '123456'
+          @user.valid?
+          expect(@user.errors.full_messages).to include('Password 英語、数字 それぞれ1文字以上を入力してください')
+        end
+
+        it 'passwordが半角でない文字がある' do
+          @user.password = '123456aｐ'
+          @user.valid?
+          expect(@user.errors.full_messages).to include('Password 英語、数字 それぞれ1文字以上を入力してください')
+        end
+
+        it 'passwordとpassword確認が不一致' do
+          @user.password_confirmation = 'a12345'
+          @user.valid?
+          expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+        end
+
+        it '名字が空' do
+          @user.family_name = nil
+          @user.valid?
+          expect(@user.errors.full_messages).to include("Family name can't be blank")
+        end
+
+        it '名字が漢字、全角ひら、全角カナで入力できていない' do
+          @user.family_name = 'myouji'
+          @user.valid?
+          expect(@user.errors.full_messages).to include('Family name 漢字、全角ひらがな、全角カタカナ を入力してください')
+        end
+
+        it '名前が空' do
+          @user.first_name = nil
+          @user.valid?
+          expect(@user.errors.full_messages).to include("First name can't be blank")
+        end
+
+        it '名前が漢字、全角ひら、全角カナで入力できていない' do
+          @user.first_name = 'abc'
+          @user.valid?
+          expect(@user.errors.full_messages).to include('First name 漢字、全角ひらがな、全角カタカナ を入力してください')
+        end
+
+        it '名字カナが空' do
+          @user.family_name_kana = nil
+          @user.valid?
+          expect(@user.errors.full_messages).to include("Family name kana can't be blank")
+        end
+
+        it '名字カナが全角カナで入力できていない' do
+          @user.family_name_kana = 'abc'
+          @user.valid?
+          expect(@user.errors.full_messages).to include('Family name kana 全角カタカナ を入力してください')
+        end
+
+        it '名前カナが空' do
+          @user.first_name_kana = nil
+          @user.valid?
+          expect(@user.errors.full_messages).to include("First name kana can't be blank")
+        end
+
+        it '名前が全角カナで入力できていない' do
+          @user.first_name_kana = 'abc'
+          @user.valid?
+          expect(@user.errors.full_messages).to include('First name kana 全角カタカナ を入力してください')
+        end
+
+        it '生年月日が空' do
+          @user.birthday = nil
+          @user.valid?
+          expect(@user.errors.full_messages).to include("Birthday can't be blank")
+        end
+
+        it '生年月日が日付でない文字列' do
+          @user.birthday = '123'
+          @user.valid?
+          expect(@user.errors.full_messages).to include("Birthday can't be blank")
+        end
       end
     end
-
-
-  end
-
   end
 end
-
-=begin ○新規登録がうまくいくとき
-①nicknameとemail,password,password確認、名字、名前、名字カナ、名前カナ、生年月日が存在
-
-○新規登録できない。
-・①がどれか一つでも空
-・emailの一意性
-・emailに@ない
-・password 5文字以下
-・password 英数混合
-・password 確認との一致
-・名字 半角文字
-・名字 全角英数字記号
-・名前 半角文字
-・名前 全角英字数字記号
-・birthday 数字でない 
-=end
