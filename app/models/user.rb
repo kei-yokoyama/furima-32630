f752ba891c999aa06f6ee902881d873c32f42ec9
kei-yokoyama <74888936+kei-yokoyama@users.noreply.github.com>
2020-12-19 15:13:25 +0900

    bundle exec rubocop -a

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,16 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   with_options presence: true do
-  validates :nickname 
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: '英語、数字 それぞれ1文字以上を半角入力してください'}
-  with_options format: {with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '漢字、全角ひらがな、全角カタカナ を入力してください'} do 
-    validates :family_name
-    validates :first_name
-  end
-  with_options format: {with: /\A([ァ-ン]|ー)+\z/, message: '全角カタカナ を入力してください'} do
-    validates :family_name_kana
-    validates :first_name_kana
-  end
-  validates :birthday
+    validates :nickname
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: '英語、数字 それぞれ1文字以上を半角入力してください' }
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '漢字、全角ひらがな、全角カタカナ を入力してください' } do
+      validates :family_name
+      validates :first_name
+    end
+    with_options format: { with: /\A([ァ-ン]|ー)+\z/, message: '全角カタカナ を入力してください' } do
+      validates :family_name_kana
+      validates :first_name_kana
+    end
+    validates :birthday
   end
 end
