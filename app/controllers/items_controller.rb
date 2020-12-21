7f4d20b8930bd60f8c812cd73c4d9a1746e01fb3
kei-yokoyama <74888936+kei-yokoyama@users.noreply.github.com>
2020-12-21 16:10:49 +0900

    商品出品機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,44 @@
 class ItemsController < ApplicationController
+  before_action :move_to_log_in, except: [:index, :show]
+
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(items_params)
+    if Item.create(items_params).valid?
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def show
+  end
+
+  private
+
+  def move_to_log_in
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+  end
+
+  def items_params
+    params.require(:item).permit(:name,:description,:category_id,:condition_id,:prefecture_id,:shipping_payer_id,:shipping_duration_id,:image,:price).merge(user_id: current_user.id)
+  end
+
 end
---
+++ app/models/item.rb
@@ -0,0 +1,24 @@
+class Item < ApplicationRecord
+  
+  with_options presence: true do
+    validates :image
+    validates :name, length: {maximum: 40, message: '40文字以内にしてください' }
+    validates :description, length: {maximum: 1000, message: '1000文字以内にしてください' }
+    validates :category
+    validates :condition
+    validates :prefecture
+    validates :shipping_duration
+    validates :shipping_payer
+    validates :price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: '(金額)を¥300 ~ 9,999,999 内にしてください' }, format: { with: /\A[0-9]+\z/, message: '(金額)を半角入力してください' }
+
+  end
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :prefecture
+  belongs_to :shipping_duration
+  belongs_to :shipping_payer
+  has_one_attached :image
+  belongs_to :user
+end
