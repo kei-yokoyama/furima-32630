36ba2df9b479262c0f86c612fc5a0a83ad324280
kei-yokoyama <74888936+kei-yokoyama@users.noreply.github.com>
2020-12-24 15:38:39 +0900

    レビュー１　修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :move_to_log_in, except: [:index, :show]
   before_action :move_to_root_path, only: [:edit]
+  before_action :set_item, only: [:edit, :show, :update]
 
   def index
     @items = Item.all.includes(:user).order('created_at DESC')
@@ -20,11 +21,11 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.find(params[:id])
+
   end
 
   def update
-    @item = Item.find(params[:id])
+
     if @item.update(items_params)
       redirect_to root_path
     else
@@ -36,10 +37,11 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+
   end
 
   private
+
 
   def move_to_log_in
     redirect_to new_user_session_path unless user_signed_in?
@@ -54,4 +56,9 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name, :description, :category_id, :condition_id, :prefecture_id, :shipping_payer_id,
                                  :shipping_duration_id, :image, :price).merge(user_id: current_user.id)
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
 end
